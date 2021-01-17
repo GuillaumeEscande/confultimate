717ac258fe2d29f39c605a04f70985c3257d4a1d
Guillaume Escande <escande.guillaume@gmail.com>
2021-01-17 22:38:07 +0100

    set version 0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ INSTALL_REQUIRES = (HERE / "requirements.txt").read_text().splitlines()
 TESTS_REQUIRE = (HERE / "test-requirements.txt").read_text().splitlines()[1:]
 
 NAME = "confultimate"
-VERSION = "0.1.1"
+VERSION = "0.2.0"
 
 setup(
     name=NAME,
