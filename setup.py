ee7443d62ca7f58c62b5db8004d8c64c571d322d
Guillaume Escande <escande.guillaume@gmail.com>
2021-01-17 23:05:39 +0100

    update main cicd

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ INSTALL_REQUIRES = (HERE / "requirements.txt").read_text().splitlines()
 TESTS_REQUIRE = (HERE / "test-requirements.txt").read_text().splitlines()[1:]
 
 NAME = "confultimate"
-VERSION = "0.2.1"
+VERSION = "0.2.2"
 
 setup(
     name=NAME,
