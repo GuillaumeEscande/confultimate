af3054952a6d44f3dcc7b8fe932e87f762f29528
Guillaume Escande <escande.guillaume@gmail.com>
2021-01-17 22:24:19 +0100

    add github integration

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
+import pathlib
 from setuptools import setup, find_packages
 
+HERE = pathlib.Path(__file__).parent
+INSTALL_REQUIRES = (HERE / "requirements.txt").read_text().splitlines()
+TESTS_REQUIRE = (HERE / "test-requirements.txt").read_text().splitlines()[1:]
+
 NAME = "confultimate"
-VERSION = "0.1.0"
-REQUIRES = ["pyyaml==5.3.1", "jsonmerge==1.7.0"]
-TESTS_REQUIRES = ["pytest==5.4.3", "pytest-cov==2.10.0", "pytest-resource-path==1.2.1"]
+VERSION = "0.1.1"
 
 setup(
     name=NAME,
@@ -17,8 +19,8 @@ setup(
     author_email="escande.guillaume@gmail.com",
     url="https://github.com/GuillaumeEscande/confultimate",
     keywords=["conf"],
-    install_requires=REQUIRES,
-    tests_require=TESTS_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
     packages=find_packages(),
     package_data={},
     include_package_data=True,
